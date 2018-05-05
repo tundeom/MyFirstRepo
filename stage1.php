468c15c0cbb1d58415720292e46881ac42756607
tundeom <39005222+tundeom@users.noreply.github.com>
2018-05-05 13:15:05 +0100

    Add files via upload

---
+++ stage1.php
@@ -0,0 +1,28 @@
+
+
+<!DOCTYPE Html>
+<html>
+  <head>
+    <link href="./stage1.css" type="text/css" rel= "stylesheet" >
+  </head>
+
+
+  <body>
+  
+    <h1> HOTELS NG STAGE ONE TASK <h1>
+
+
+  <div class="DateAndTime">   
+    <?php
+        echo date("d/m/Y") . "<br>". "<br>";
+  
+        date_default_timezone_set("Africa/Lagos");
+        echo  date("h:i:sa");
+    ?>
+    
+   </div>
+
+  </body>
+
+
+</html>
